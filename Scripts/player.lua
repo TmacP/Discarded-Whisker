4868bbdcaddff81c0e4766f6c39d8858702eaf30
Tristen MacPherson <trm003@mail.usask.ca>
2023-12-21 16:49:49 -0600

    fix random seed bug

--- Scripts/player.lua
+++ Scripts/player.lua
@@ -16,8 +16,19 @@ function player.create()
 end
 
 function player.drawCard(player)
+	math.randomseed(os.time()) -- set the seed to make non deterministic
+
 	if #player.deck > 0 then
 		local randomIndex = math.random(1, #player.deck)
+		
+		-- pop a few random off to keep it random
+		math.random(1, #player.deck)
+		math.random(1, #player.deck)
+		math.random(1, #player.deck)
+		math.random(1, #player.deck)
+		math.random(1, #player.deck)
+		-- continue
+		
 		local drawnCard = table.remove(player.deck, randomIndex)
 		table.insert(player.hand, drawnCard)
 		return drawnCard
