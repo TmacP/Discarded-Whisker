85a7d1d7f0c0c62eac4cf0d157f8411377ea4329
Tristen MacPherson <trm003@mail.usask.ca>
2023-12-21 15:30:19 -0600

    starting end turn button

---
+++ gui/gui.go
@@ -0,0 +1,15 @@
+components {
+  id: "end_turn"
+  component: "/gui/end_turn.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
