7b7f5a220a4160d3a011a902f840ce834d74b18b
Tristen MacPherson <trm003@mail.usask.ca>
2024-02-09 10:54:38 -0600

    bug fix when player wins

---
+++ Collections/map/gui/gui.go
@@ -0,0 +1,15 @@
+components {
+  id: "map"
+  component: "/Collections/map/gui/map.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
