2c48277a1251e0211f17f1add9a1f32543b9ada7
Tristen MacPherson <trm003@mail.usask.ca>
2024-02-09 17:13:56 -0600

    Added title screen

--- Collections/main/controller.go
+++ Collections/main/controller.go
@@ -58,3 +58,18 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "title"
+  component: "/Collections/title/title.collectionproxy"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
